7e974b8a12c23c6fed88ec0faaf5663bf6bd850c
aviavissar <aviavissar@gmail.com>
2021-04-06 02:13:53 +0300

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 export default {
-    DEFAULT_SEARCH_CITY: "Tel Aviv",
-    WEATHER_API_KEY: "AEvJItzx62qMCozpuB0nGCsf3izvCzZa" , 
-    DEFAULT_KEY_CITY:"215854", 
+    DEFAULT_SEARCH_CITY: process.env.DEFAULT_SEARCH_CITY,
+    WEATHER_API_KEY: process.env.WEATHER_API_KEY, 
+    DEFAULT_KEY_CITY:process.env.DEFAULT_KEY_CITY, 
     HEROKU:"https://cors-anywhere.herokuapp.com/"
 }
